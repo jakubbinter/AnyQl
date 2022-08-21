3114dbda4eabd0d8853cce8577f7a9f1e3075e8b
jakubbinter <kuba.binter@gmail.com>
2022-08-21 17:17:34 +0200

    updated example program

--- Examples/JsonExample/Program.cs
+++ Examples/JsonExample/Program.cs
@@ -1,34 +1,22 @@
 ﻿using AnyQl;
 
+//sample json string that could be response from an api with cooking recepies
+//please do not use this as real cooking recepie, as I just made it up and it probably won't really work
 string Json = @"{
-  'Stores': [
-    'Lambton Quay',
-    'Willis Street'
-  ],
-  'Manufacturers': [
-    {
-      'Name': 'Acme Co',
-      'Products': [
+    'count':1,
+    'items':[
         {
-          'Name': 'Anvil',
-          'Price': 50
+            'name':'potatoe chips',
+            'ingredients':['potatoes','salt'],
+            'cooking_process':'wash your potatoes, cut them to small slices, put bit of salt on them and put them to oven for 30 minutes at 200°C'
         }
-      ]
-    },
-    {
-      'Name': 'Contoso',
-      'Products': [
-        {
-          'Name': 'Elbow Grease',
-          'Price': 99.95
-        },
-        {
-          'Name': 'Headlight Fluid',
-          'Price': 4
-        }
-      ]
-    }
-  ]
+    ]
 }";
-Console.WriteLine(SqlGenerator.Generate(Json, new string[] { "Stores[0]", "Manufacturers[1].Products[0].Price" }, "MyTable", FileFormat.Json, SqlStatementType.INSERT));
-Console.WriteLine(SqlGenerator.Generate(Json, new string[] { "Stores[0]", "Manufacturers[1].Products[0].Price" }, new string[] { "Store", "Price" }, "MyTable", FileFormat.Json, SqlStatementType.UPDATE));
+//sample insert string with implicit column names
+string? sampleInsert = SqlGenerator.Generate(Json, new string[] { "items[0].name", "items[0].cooking_process" }, "MyRecepies", FileFormat.Json, SqlStatementType.INSERT);
+
+//sample update string with explicit column names
+string? sampleUpdate = SqlGenerator.Generate(Json, new string[] { "items[0].name", "items[0].cooking_process" }, new string[] { "RecepieName", "procedure" }, "MyRecepies", FileFormat.Json, SqlStatementType.UPDATE);
+
+Console.WriteLine(sampleInsert);
+Console.WriteLine(sampleUpdate);
